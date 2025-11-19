d1b1830af49d7b9005f35b9562a0e6faa53139ba
natsukokuwahara <natsuko.kuwahara@gmail.com>
2025-11-19 11:59:20 -0500

    Add files via upload

---
+++ story_content/5zZAd54nrdR_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5zZAd54nrdR_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.105.35604.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:01.690%0D%0AOK.%20here\'s%20the%20last%20one!%0D%0A%0D%0A"}]})
